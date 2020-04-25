25ba009a889067b9488ec51712b0d4afc4b35f7c
basantpatidar <basantpatidar33@gmail.com>
2020-04-24 20:51:02 -0400

    cases

---
+++ src/main/java/com/spring/boot/covid/model/CovidCases.java
@@ -0,0 +1,17 @@
+package com.spring.boot.covid.model;
+
+public class CovidCases {
+
+	
+	CovidCaseDTO[] covidCase;
+
+	public CovidCaseDTO[] getCovidCase() {
+		return covidCase;
+	}
+
+	public void setCovidCase(CovidCaseDTO[] covidCase) {
+		this.covidCase = covidCase;
+	}
+	
+	
+}
