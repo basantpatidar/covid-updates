0de9548fbd18a533e5f48f658db4851d942905eb
basantpatidar <basantpatidar33@gmail.com>
2020-04-26 21:37:42 -0400

    tests

---
+++ src/test/java/com/spring/boot/covid/CoronavirusUpdatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.covid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusUpdatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
